5f519dd1be889a361e0df867d15775e1e984f08e
JuliaRajn <rajnjulia@gmail.com>
2024-11-04 15:31:32 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,7 @@
+def test_function():
+  def inner_function():
+    print("Я в области видимости функции test_function")
+
+  inner_function() # Вызов inner_function внутри test_function
+
+test_function()
